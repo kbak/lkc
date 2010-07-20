4901cba253749fabf60b4101789fc69493e42ba9
Karim Hamdan <karim@hp-lt.(none)>
2010-07-20 11:09:16 -0400

    added FcdChooseFile (file chooser class) so that we won't duplicate code modified WindowConfig to add Open file in the tool bar button

--- code/app/src/ca/uwaterloo/lkc/WindowConfig.java
+++ code/app/src/ca/uwaterloo/lkc/WindowConfig.java
@@ -7,12 +7,15 @@ import org.gnome.gdk.Event;
 import org.gnome.glade.Glade;
 import org.gnome.glade.XML;
 import org.gnome.gtk.Gtk;
+import org.gnome.gtk.ResponseType;
+import org.gnome.gtk.ToolButton;
 import org.gnome.gtk.Widget;
 import org.gnome.gtk.Window;
 
 public class WindowConfig {
     
    public final Window w;
+   public final ToolButton tbtnOpen;
     
     WindowConfig(final String gladeFile, URI configFile) throws FileNotFoundException
     {
@@ -28,6 +31,33 @@ public class WindowConfig {
                 return false;
             }
         });
+        
+        // Adding evens for tool bar buttons
+        tbtnOpen = (ToolButton) xmlWndConfig.getWidget("tbtnOpen");
+        tbtnOpen.connect(new ToolButton.Clicked() {
+			
+			@Override
+			public void onClicked(ToolButton arg0) {
+				try {
+					FcdChooseFile fcdChooseFile;
+					fcdChooseFile = new FcdChooseFile(gladeFile);
+					ResponseType responseType = fcdChooseFile.run();
+					
+					/*
+					 *  If a file is chosen, hide the current WindowConfig,
+					 *  instantiate a new one with the new configuration.
+					 */
+					if(responseType == ResponseType.OK) {
+						w.hide();
+						new WindowConfig(gladeFile, fcdChooseFile.getURI()).w.show();
+					} else if(responseType == ResponseType.CANCEL) {
+						fcdChooseFile.hide();
+					}
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		});
         
     }
 
